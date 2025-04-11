dbad3d1f5d21651bfd159274731bb9a92f397c95
mictnic <914416506@qq.com>
2025-04-11 15:47:48 +0800

    test1111

--- rbtreeview.cpp
+++ rbtreeview.cpp
@@ -2,18 +2,37 @@
 
 #include <deque>
 #include <QPainter>
+#include <QPushButton>
+#include <QSpinBox>
 
 rbtreeview::rbtreeview(QWidget* parent /*= nullptr*/, Qt::WindowFlags f /*= Qt::WindowFlags()*/)
 	: QWidget(parent, f)
 {
 	setFixedSize(1000, 800);
 
+	m_pBtn = new QPushButton(this);
+	m_pBtn->setGeometry(QRect(QPoint(0,0), QPoint(50,20)));
+	m_pSbx = new QSpinBox(this);
+	m_pSbx->setGeometry(QRect(QPoint(50, 0), QPoint(100, 20)));
+
 	m_rbTree.addNode(5);
 	m_rbTree.addNode(3);
 	m_rbTree.addNode(2);
-	m_rbTree.addNode(8);
-	m_rbTree.addNode(7);
-	m_rbTree.addNode(6);
+// 	m_rbTree.addNode(8);
+// 	m_rbTree.addNode(7);
+// 	m_rbTree.addNode(6);
+// 	m_rbTree.addNode(9);
+// 	m_rbTree.addNode(0);
+// 	m_rbTree.addNode(4);
+// 	m_rbTree.addNode(1);
+
+	connect(m_pBtn, SIGNAL(clicked()), SLOT(onAdd()));
+}
+
+void rbtreeview::onAdd()
+{
+	m_rbTree.addNode(m_pSbx->value());
+	update();
 }
 
 void rbtreeview::paintEvent(QPaintEvent* event)
@@ -33,13 +52,13 @@ void rbtreeview::paintEvent(QPaintEvent* event)
 		return;
 	}
 
-	std::map<rbnode*, int> node2Fid;
+	std::map<rbnode*, std::pair<int, int> > node2Fid;
 
 	int row = 0;
 	int col = 0;
 	std::deque<rbnode*> nodes;
 	std::deque<rbnode*> nextNodes;
-	int hbeg = 20;
+	int hbeg = 50;
 	int woff = width() / 2 - (pow(2, depth) - 1) * 20 / 2;
 	nodes.push_back(m_rbTree.getRoot());
 	while (!nodes.empty() || !nextNodes.empty())
@@ -67,6 +86,7 @@ void rbtreeview::paintEvent(QPaintEvent* event)
 		}
 
 		int nFid = 0;
+		int pcen = 0;
 		if (nullptr == node->parent)
 		{
 			nFid = pow(2, depth - 1) / 2;
@@ -75,21 +95,26 @@ void rbtreeview::paintEvent(QPaintEvent* event)
 		{
 			auto ite = node2Fid.find(node->parent);
 			assert(ite != node2Fid.end());
-			auto nPFid = ite->second;
+			auto nPFid = ite->second.first;
 			nFid = nPFid + (node == node->parent->left ? -1 : 1) * ((nPFid + 1) / 2);
+			pcen = ite->second.second;
 		}
 
-		node2Fid[node] = nFid;
 		nextNodes.push_back(node->left);
 		nextNodes.push_back(node->right);
 
 		auto cen = woff + 10 + nFid * 20;
 		auto hig = row * 40 + hbeg;
 
+		node2Fid[node] = std::make_pair(nFid, cen);
+
 		painter.setBrush(QBrush(node->red ? Qt::red : Qt::lightGray));
 		painter.drawEllipse(QRect(QPoint(cen - 10, hig - 10), QPoint(cen + 10, hig + 10)));
 		painter.drawText(QRect(QPoint(cen - 10, hig - 10), QPoint(cen + 10, hig + 10)), Qt::AlignCenter, QString::number(node->val));
-
+		if (nullptr != node->parent)
+		{
+			painter.drawLine(QPoint(cen, hig), QPoint(pcen, hig - 40));
+		}
 		col++;
 	}
 }
--- rbtreeview.h
+++ rbtreeview.h
@@ -3,12 +3,23 @@
 #include <QWidget>
 #include "rbtree.h"
 
+class QPushButton;
+class QSpinBox;
+
 class rbtreeview : public QWidget
 {
 	Q_OBJECT
 
 public:
 	explicit rbtreeview(QWidget* parent = nullptr, Qt::WindowFlags f = Qt::WindowFlags());
+
+protected slots:
+	void onAdd();
+
+protected:
+	QPushButton* m_pBtn;
+	QSpinBox* m_pSbx;
+
 
 protected:
 	void paintEvent(QPaintEvent* event) override;
